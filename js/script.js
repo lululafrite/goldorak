abc1f3675f592ecc98b5e677f1b561745a748d1d
lululafrite <ludovic.follaco@free.fr>
2023-12-28 13:44:08 +0100

    Version 2023-12-28_13:44 | Ajout du Jeu Goldorak Go pour évaluation javascript

---
+++ js/script.js
@@ -0,0 +1,173 @@
+var playerName;
+var attempts = 0;
+var maxNumber;
+var secretNumber;
+var guessedNumbers = [];
+
+var gameContainer = document.getElementById("game-container");
+var contenuHtmlActuel;
+var ajoutBaliseHtml;
+var ajoutBaliseHtml_2;
+var propositionEffectuée;
+
+// Fonction pour commencer le jeu
+function startGame() {
+    // Récupérer les valeurs du formulaire
+    playerName = document.getElementById("playerName").value;
+    var difficultyElements = parseInt(document.querySelector('input[name=difficulty]:checked').value);
+
+    // Trouver la difficulté sélectionnée
+    //difficultyElements = parseInt(difficultyElements);
+
+    // Vérifier la validité de la difficulté
+    if (isNaN(difficultyElements) || difficultyElements < 1 || difficultyElements > 3) {
+        alert("Choix de difficulté non valide. Le jeu va se fermer.");
+    } else {
+        // Définir la plage de nombres en fonction de la difficulté
+        switch (difficultyElements) {
+            case 1:
+                maxNumber = 10;
+                break;
+            case 2:
+                maxNumber = 100;
+                break;
+            case 3:
+                maxNumber = 1000;
+                break;
+            default:
+                break;
+        }
+
+        // Générer le nombre mystère
+        secretNumber = Math.floor(Math.random() * maxNumber) + 1;
+
+        // Afficher le jeu
+        displayGame();
+    }
+}
+
+// Afficher le jeu
+function displayGame() {
+    gameContainer.innerHTML = `
+        <div class=" mb-3 ms-3 me-auto pe-3">
+            <h2>Bon courage, ${playerName}!<br>Trouve le nombre de Golgote, il peut être de 1 à ${maxNumber}</h2>
+            <form id="guess-form">
+                <label for="userGuess">Quel est le nombre de Golgote ?</label>
+                <input type="number" id="userGuess" name="userGuess" min="1" max="${maxNumber}" required onkeydown="if(event.keyCode==13) makeGuess()">
+                <br>
+                <button class="btn btn-lg btn-primary" id="guessButton" type="button" onclick="makeGuess()">Proposer</button>
+            </form>
+        </div>
+    `;
+    contenuHtmlActuel = gameContainer.innerHTML;
+
+    // Ajoute un gestionnaire d'événements pour la touche "Entrée" sur le bouton "Proposer"
+    document.getElementById("guessButton").addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) {
+            makeGuess();
+        }
+    });
+
+    // Ajoute un gestionnaire d'événements pour la touche "Entrée" dans le champ de texte
+    document.getElementById("userGuess").addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) {
+            makeGuess();
+        }
+    });
+}
+
+// Fonction pour faire une proposition
+function makeGuess() {
+    var userGuess = parseInt(document.getElementById("userGuess").value);
+
+    // Vérifier la validité de la saisie
+    if (isNaN(userGuess) || userGuess < 1 || userGuess > maxNumber) {
+        alert("Saisie non valide. Réessaie.");
+    } else {
+        // Incrémenter le nombre de tentatives
+        attempts++;
+
+        // Ajouter la réponse à la liste des réponses déjà effectuées
+        guessedNumbers.push(userGuess);
+
+        // Vérifier la devinette
+        if (userGuess < secretNumber) {
+            customAlert("Trop bas! Essaie encore.");
+            gameContainer.innerHTML = contenuHtmlActuel + displayResults();
+        } else if (userGuess > secretNumber) {
+            customAlert("Trop haut! Essaie encore.");
+            gameContainer.innerHTML = contenuHtmlActuel + displayResults();
+        } else {
+            // Le joueur a deviné correctement
+            var messageGagnant = "<h2 class='m-3'>Bravo, " + playerName + "!</h2><h3 class='m-3'>Tu as trouvé le nombre de Golgote en " + attempts + " tentatives.</h3>";
+            
+            ajoutBaliseHtml = `
+                <p class="m-3" >` + messageGagnant + `</p>
+                <img class="m-3" src="img/gagne.gif" alt="J'ai gagné">
+                <br>
+                `+ displayResults(); +`
+                <br>
+            `;
+            ajoutBaliseHtml_2 = `
+                <button class="btn btn-lg btn-primary ms-3 me-auto mb-3" type="button" onclick="ouvrirJeux()">Nouvelle partie</button>
+            `;
+            gameContainer.innerHTML = ajoutBaliseHtml + ajoutBaliseHtml_2;
+
+        }
+    }
+}
+
+// Afficher les résultats
+function displayResults() {
+    propositionEffectuée = "<p class='m-3'>Réponses déjà proposées : " + guessedNumbers.join(", ") + "</p>";
+    return propositionEffectuée;
+}
+
+function ouvrirJeux() {
+    window.location.href = "goldorakgo.html";
+}
+
+// Fonction personnalisée pour alert
+function customAlert(message) {
+    alert(message);
+}
+
+// Fonction personnalisée pour prompt
+function customPrompt(message) {
+    return prompt(message);
+}
+
+// Ajoute un gestionnaire d'événements pour la touche "Entrée" lors de l'utilisation de prompt
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13 && document.activeElement.tagName.toLowerCase() === "input" && document.activeElement.type === "text") {
+        // Intercepte la touche "Entrée" dans le champ de texte de prompt
+        event.preventDefault();
+        customPromptInput();
+    }
+});
+
+// Fonction personnalisée pour intercepter la touche "Entrée" dans le champ de texte de prompt
+function customPromptInput() {
+    // Récupère la valeur actuelle du champ de texte
+    var inputText = document.activeElement.value;
+
+    // Ferme la fenêtre prompt
+    customPromptClose();
+
+    // Utilise la valeur du champ de texte comme si la touche "Entrée" avait été pressée
+    customPromptReturnValue(inputText);
+}
+
+// Fonction personnalisée pour fermer la fenêtre prompt
+function customPromptClose() {
+    document.activeElement.blur();
+}
+
+// Fonction personnalisée pour renvoyer la valeur à partir de prompt
+function customPromptReturnValue(value) {
+    // Utilisez la valeur récupérée comme vous le feriez normalement dans le code
+    console.log("Valeur de prompt : " + value);
+}
+
+// Redéfinition de la fonction prompt pour utiliser notre version personnalisée
+window.prompt = customPrompt;
